15bcdaa9bbc52560ab67158479b32de9b798a639
Sergio-Sena <senanetworker@gmail.com>
2022-08-08 12:31:24 -0300

    Add update

---
+++ Gerador.py
@@ -0,0 +1,21 @@
+#Gerador de senha
+#Importar Biblioteca Random para Randomizar
+import random
+#variavel para Letras maiúscula
+para_maiúscula = 'ABCDEFGHIJKLMNOPQRSTUVXWYZ'
+#variavel para Letras maiúscula
+para_minúsculo = 'abcdefghijklmnopqrstuvxwyz'
+#variavel para Simbolos
+para_simbolos = '!#$%&\?/'
+#variavel para Números
+para_números = '0123456789'
+#Itens para Randomizar
+use = para_minúsculo+para_maiúscula+para_números+para_simbolos
+# Digitos para senha de 8 caracteres
+tamanho_da_senha = 8
+#Variavel com a senha e o join para juntar as caracteres randomizados  
+senha = ''.join(random.sample(use,tamanho_da_senha))
+#print com a senha gerada
+print('#-'*30)
+print('Sua senha é {:-^30}'.format(senha))
+print('#-'*30)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Gerador-de-senha
+div<>
+    Arquivo com a descriçao e explicaçao para gerar senha de 8 digitos.
+    div</>
